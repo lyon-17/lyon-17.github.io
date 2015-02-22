af4a265618fdff0023990ad9dcad872ad914a074
lyon-17 <lyon-17@hotmail.com>
2015-02-22 21:46:46 +0100

    portfolio

---
+++ portfolio/js/index.js
@@ -0,0 +1,34 @@
+
+
+$(function(){
+	jQuery.fn.cambioColor = function() {
+		this.each(function(){
+			elem = $(this);
+			elem.css('color','rgba(59,58,62,1)');
+			elem.fadeOut(1000, function(){
+				$(this).fadeIn(1000);
+				elem.css('color','rgba(115,136,117,1)');
+			});
+		});
+	return this;
+	}; 
+
+	$('article').mouseenter(function(event){
+		event.preventDefault();
+		$('p',this).css('background-color','rgba(43,43,41,0.9)');
+		//$('p',this).css('width','25%');
+		$('p',this).fadeIn(500);
+	});
+	$('article').mouseout(function(event){
+		event.preventDefault();
+		$('p',this).css('background-color','none');
+		$('p',this).fadeOut(1000);
+	});
+
+	$('#tabs').tabs();
+	$('h4').cambioColor();
+
+	$( "#datepicker" ).datepicker({
+	inline: false
+});
+});
